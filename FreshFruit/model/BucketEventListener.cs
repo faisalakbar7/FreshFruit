9d61b84f02553e432dbd58c0a29b23c71ae97c93
faisalakbar7 <nervegear08@gmail.com>
2021-10-28 11:50:02 +0700

    Add project files.

---
+++ FreshFruit/model/BucketEventListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.model
+{
+    interface BucketEventListener
+    {
+        void onSucced(string message);
+        void onFailed(string message);
+    }
+}
